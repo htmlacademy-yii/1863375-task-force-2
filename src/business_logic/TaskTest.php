fb5d639a6c8f09298eaab2cf9053bf6b8d10aad0
AnnaNazz <88041207+AnnaNazz@users.noreply.github.com>
2021-10-12 22:02:16 +0700

    Update TaskTest.php

--- src/business_logic/TaskTest.php
+++ src/business_logic/TaskTest.php
@@ -1,6 +1,6 @@
 <?php
     require_once "../../vendor/autoload.php";
-   // use TaskForce\Task;
+
     $task = new TaskForce\business_logic\Task();
     $task->setAction('choose_a_performer');
     assert($task->getNextStatus() == TaskForce\business_logic\Task::STATUS_IN_WORK);
